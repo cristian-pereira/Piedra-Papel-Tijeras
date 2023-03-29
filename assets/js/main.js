500e3c192fd054596ede896485e4d827f193ee76
jaderRivera <riverajaderrw@gmail.com>
2023-03-29 14:23:32 -0500

    Subiendo cambios a el juego

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,9 @@
 const botones = document.querySelectorAll('.botones')
 
-const piedra = document.querySelector('.piedra')
-const papel = document.querySelector('.papel')
-const tijera = document.querySelector('.tijera')
-
-const manoUsuario = document.querySelector('.mano-usuario')
-const manoComputador = document.querySelector('.mano-computador')
-
 const puntajeUsuario = document.querySelector('.puntaje-usuario')
 const puntajeComputador = document.querySelector('.puntaje-computador')
+
+const ganador = document.querySelector('.ganador')
 
 const manos = document.querySelector('.manos')
 
@@ -30,23 +25,94 @@ const animation = () => {
 }
 
 const jugar = () => {
-    
     botones.forEach(boton => {
         boton.addEventListener('click', e => {
 
             const opciones = ['piedra', 'papel', 'tijera'];
             let opcion  = e.target.parentNode.classList[1];
             let random = Math.floor(Math.random() * 3);
+
+            
             manos.innerHTML = `
-                <img class="mano-usuario" src="assets/img/piedra_ada.png" alt="imagen de piedra">
-                <img class="mano-computador" src="assets/img/piedra_computadora.png" alt="imagen de piedra">
+            <img class="mano-usuario" src="assets/img/piedra_ada.png" alt="imagen de piedra">
+            <img class="mano-computador" src="assets/img/piedra_computadora.png" alt="imagen de piedra">
             `;
             setTimeout(() => {
                 manos.innerHTML = `
                     <img class="mano-usuario" src="assets/img/${opcion}_ada.png" alt="imagen de piedra">
                     <img class="mano-computador" src="assets/img/${opciones[random]}_computadora.png" alt="imagen de piedra">
                 `;
+                setTimeout(() => {
+                    validacion(opcion, opciones[random])
+                    gana()
+                }, 500);
             }, 2000);
         })
     })
+}
+
+const validacion = (opcion, opciones) => {
+    if ((opcion === 'piedra' && opciones === 'tijera')
+     || (opcion === 'papel' && opciones === 'piedra')
+     || (opcion === 'rijera' && opciones === 'papel')) {
+        Swal.fire({
+            icon: 'success',
+            title: 'El jugador Gana',
+            text: 'Obtienes 1 punto',
+            width: '50em',
+            confirmButtonText: 'OK!',
+        })
+        puntajeUsuario.innerHTML = parseInt(puntajeUsuario.innerHTML) + 1
+    }else if (opcion === opciones ){
+        Swal.fire({
+            icon: 'info',
+            title: 'Estan empatados',
+            width: '50em',
+            confirmButtonText: 'OK!',
+        })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'La computadora gana',
+            text: 'Obtiene 1 punto',
+            width: '50em',
+            confirmButtonText: 'OK!',
+        })
+        puntajeComputador.innerHTML = parseInt(puntajeComputador.innerHTML) + 1
+
+    }
+}
+
+const gana = () => {
+    if ( parseInt(puntajeComputador.innerHTML) === 3) {
+        Swal.fire({
+            icon: 'error',
+            title: '¡Has perdido!',
+            text: ':(',
+            width: '50em',
+            confirmButtonText: 'OK!',
+        })
+        ganador.innerHTML = `
+            <p>La computadora gana :(</p>
+        `
+    }else if(parseInt(puntajeUsuario.innerHTML) === 3){
+        Swal.fire({
+            icon: 'success',
+            title: '¡Has Ganado!',
+            text: ':D',
+            width: '50em',
+            confirmButtonText: 'OK!',
+        })
+        ganador.innerHTML = `
+            <p>¡Ganaste! :D</p>
+        `
+    }
+
+    if ((parseInt(puntajeComputador.innerHTML) === 3) || (parseInt(puntajeUsuario.innerHTML) === 3)) {
+       setTimeout(() => {
+           puntajeUsuario.innerHTML = 0;
+           puntajeComputador.innerHTML = 0;
+           ganador.innerHTML = ''
+       }, 3000);
+    }
 }
